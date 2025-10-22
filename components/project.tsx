dfd87b196d7e493756486beaff9924d544d3fa6e
Saidul Badhon <31236650+SaidulBadhon@users.noreply.github.com>
2025-10-22 18:01:17 +0600

    Add shortDescription to projects and update project data
    Introduced a shortDescription field to the Project component and updated the projectsData to include new and revised project entries with short descriptions. The component now displays shortDescription if available, falling back to description otherwise.

--- components/project.tsx
+++ components/project.tsx
@@ -12,6 +12,7 @@ export default function Project({
   slug,
   title,
   description,
+  shortDescription,
   tags,
   imageUrl,
   coverArt,
@@ -38,7 +39,7 @@ export default function Project({
           <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
             <h3 className="text-2xl font-semibold">{title}</h3>
             <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 max5Lines">
-              {description}
+              {shortDescription || description}
             </p>
             <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
               {tags.map((tag, index) => (
